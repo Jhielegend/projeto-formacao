aee46b2b7fa2eb77b464f8c658c15fb665bb13c5
KamillaKempim <kamillamidia@gmail.com>
2025-08-30 20:37:07 -0400

    Adicionei o footer, e a pasta do CSS

---
+++ _partes/_footer.php
@@ -0,0 +1,14 @@
+<div class="r-linha">
+    <div class="r-linha-item">
+        <img src="Images/.png" alt="Logo da Empresa">
+        <p>Contatos:</p>
+        <p><i class="bi bi-telephone"></i> (99) 999999-9999</p>
+    </div>
+    <div class="r-linha-item rede-social">
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com//" class="instagram" target="_blank"><i
+                class="bi bi-instagram"></i></a>
+        <a href="https://wa.me/559999999999" class="whatsapp" target="_blank"><i class="bi bi-whatsapp"></i></a>
+    </div>
+</div>
+<p>&copy; 2025 CoopAgro, Todos os direitos reservados</p>
